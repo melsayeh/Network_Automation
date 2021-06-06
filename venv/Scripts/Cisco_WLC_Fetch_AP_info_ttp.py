88b729302778c5e4aaffc8cc4bdcf3407326376f
Mansour Elsayeh <mansour.elsayeh@gmail.com>
2021-06-07 00:40:30 +0400

    Update Cisco_WLC_Fetch_AP_info_ttp.py

--- venv/Scripts/Cisco_WLC_Fetch_AP_info_ttp.py
+++ venv/Scripts/Cisco_WLC_Fetch_AP_info_ttp.py
@@ -2,7 +2,7 @@ import ttp
 import os
 
 from ttp import ttp
-parser = ttp(data="D:/Python/Export AP info from Cisco WLC 5760/AP_raw_data.txt", template="D:/Python/Export AP info from Cisco WLC 5760/ttp_template.txt")
+parser = ttp(data="path/to/ttp_template")
 parser.parse()
 result = parser.result(format="csv")
 print(result[0])
