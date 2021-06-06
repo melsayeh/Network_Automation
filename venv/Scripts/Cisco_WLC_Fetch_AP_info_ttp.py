225420525da935fac0d260a1176ba92ce0a31634
Mansour Elsayeh <mansour.elsayeh@gmail.com>
2021-06-07 00:40:40 +0400

    Update Cisco_WLC_Fetch_AP_info_ttp.py

--- venv/Scripts/Cisco_WLC_Fetch_AP_info_ttp.py
+++ venv/Scripts/Cisco_WLC_Fetch_AP_info_ttp.py
@@ -2,7 +2,7 @@ import ttp
 import os
 
 from ttp import ttp
-parser = ttp(data="path/to/ttp_template")
+parser = ttp(data="/path/to/ttp_template")
 parser.parse()
 result = parser.result(format="csv")
 print(result[0])
